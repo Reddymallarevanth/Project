20c43ab061a5151309f36976ef393f5e96981cee
929030 <929030@LTIN196974.cts.com>
2021-10-19 11:10:37 +0530

    first commit

---
+++ SpringDocketProject/src/main/java/com/revanth/docker/SpringDocketProjectApplication.java
@@ -0,0 +1,13 @@
+package com.revanth.docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDocketProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDocketProjectApplication.class, args);
+	}
+
+}
---
+++ SpringDocketProject/src/test/java/com/revanth/docker/SpringDocketProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revanth.docker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDocketProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
